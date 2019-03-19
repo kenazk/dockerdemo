2e5f5c7e874a7c58daa6a368b5476975c2680ea9
Kenaz <kenaz@puppetlabs.com>
2019-03-19 16:28:49 -0700

    changing this

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import socket
 
 # Connect to Redis
-redis = Redis(host="redis", port=6379, db=0, socket_connect_timeout=2, socket_timeout=2)
+redis = Redis(host="localhost", port=6379, db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
+    html = "<h3>Hello {name}!!!!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
